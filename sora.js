4a15a0364b57e89af9a094f6f84f766cb8d9f47c
presetkiether07 <presetkiether@gmail.com>
2025-04-30 15:02:03 +0800

    Update sora.js

--- sora.js
+++ sora.js
@@ -8,13 +8,27 @@ process.on('unhandledRejection', error => console.log(error));
 process.on('uncaughtException', error => console.log(error));
 
 const fs = require("fs-extra");
-const logger = require("./imports/logger");
+let logger, login, listener;
+
+try {
+  logger = require("./imports/logger");
+} catch (err) {
+  console.error("ERROR: Failed to load ./imports/logger");
+  console.error(err);
+}
+
 const figlet = require("figlet");
 const chalk = require("chalk");
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
-const login = require("./imports/login");
+
+try {
+  login = require("./imports/login");
+} catch (err) {
+  console.error("ERROR: Failed to load ./imports/login");
+  console.error(err);
+}
 
 process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
 const app = new express();
@@ -61,15 +75,21 @@ global.Data = new Object({
 
 // Main start function
 async function start() {
-  const utils = require("./utils");
-  global.utils = utils;
+  try {
+    const utils = require("./utils");
+    global.utils = utils;
+  } catch (err) {
+    console.error("ERROR: Failed to load ./utils");
+    console.error(err);
+    return;
+  }
 
   const appState = fs.readJSONSync(
     path.join(__dirname, 'json', 'state.json')
   );
 
   figlet.text("SoraV2", (err, data) => {
-    if (err) return logger.error(err);
+    if (err) return logger?.error?.(err);
 
     console.log(chalk.cyan(data));
     console.log(chalk.blue(`> Bot Name: [ ${config.botName} ]`));
@@ -78,16 +98,21 @@ async function start() {
     console.log();
 
     login({ appState }, (err, api) => {
-      if (err) return logger.error(err);
+      if (err) return logger?.error?.(err);
 
       api.setOptions(config.fcaOptions);
 
       api.listen(async (err, event) => {
-        if (err) return logger.error(err);
+        if (err) return logger?.error?.(err);
 
         if (event.senderID !== api.getCurrentUserID()) {
-          const listener = require("./imports/listener");
-          listener({ api, event });
+          try {
+            listener = require("./imports/listener");
+            listener({ api, event });
+          } catch (err) {
+            console.error("ERROR: Failed to load ./imports/listener");
+            console.error(err);
+          }
         }
       });
     });
