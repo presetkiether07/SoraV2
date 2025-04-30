f54dbcddd2f4494b0b034a2fa030515551f38c86
presetkiether07 <presetkiether@gmail.com>
2025-04-30 14:51:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const logger = require("./imports/logger");
 async function start() {
   const bot = spawn('node sora.js', {
     cwd: __dirname,
-    stdio: "inherit",
+    stdio: "inherit", // Ensures the stdio of the spawned process is directly inherited
     shell: true
   });
 
+  // Log any errors that occur during spawn
+  bot.on("error", (err) => {
+    logger.error("Error spawning bot:", err);
+  });
+
+  // Check when the bot process closes
   bot.on("close", (code) => {
     if (code === 2) {
       logger.info("Bot is Restarting...");
       start();
-    };
+    } else {
+      logger.error(`Bot crashed with exit code ${code}`);
+    }
   });
-};
+}
 
 start();
